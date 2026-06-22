953c4b1f468f3ed2d977d4167df5e7b24f7b9993
Luis Paspuezán <andres1519q@gmail.com>
2026-06-22 11:58:30 -0500

    chore: add capacitor android build

---
+++ frontend/android/app/src/main/java/com/uceconnect/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.uceconnect.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.uceconnect.app',
+  appName: 'UCEConnect',
+  webDir: 'dist'
+};
+
+export default config;
